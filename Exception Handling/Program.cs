0ce85b93e5d89e7f351e02df7127f02f6c5423fc
Prajapati <jenish.prajapati@cotiviti.com>
2023-07-13 11:09:41 +0545

    IDisposable message

--- Exception Handling/Program.cs
+++ Exception Handling/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,18 +12,38 @@ namespace Exception_Handling
         static void Main(string[] args)
         {
             
-                dynamic numerator = 29;
-                dynamic denom = 0;
+            dynamic numerator = 29;
+            dynamic denom = 10;
+            var streamReader = new StreamReader(@"C:\Users\jenish.prajapati\OneDrive - Cotiviti\Desktop\layout.txt");
+
             try
             {
                 dynamic di = numerator / denom;
+                Console.WriteLine(di);
+                
+                var content=streamReader.ReadToEnd();
+                //Console.WriteLine(content);
+
             }
+            catch (DivideByZeroException ex)    //order of catch statements maters
+            {
+                Console.WriteLine(ex);
+            }
+
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                Console.WriteLine("Can't divide by 0");
+                //Console.WriteLine("Can't divide by 0");
             }
-
+            finally
+            {
+                streamReader.Dispose();
+            }
             
         }
     }
