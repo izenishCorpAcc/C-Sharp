c9af0c4ab8972390a3978c44ce2d5736799df651
Prajapati <jenish.prajapati@cotiviti.com>
2023-07-13 10:45:21 +0545

    Error Handler

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                dynamic numerator = 29;
+                dynamic denom = 0;
+            try
+            {
+                dynamic di = numerator / denom;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Can't divide by 0");
+            }
+
+            
+        }
+    }
+}
